a94bf660bb19244bf041115d047aeb1c80a53df6
Opeyemi <yemilokoro@gmail.com>
2023-06-14 11:56:43 -0400

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,9 +9,12 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition:</strong>
               {definition.definition}
               <br />
+              <strong> Example:</strong>
               <em>{definition.example}</em>
+              {definition.synonyms}
             </p>
           </div>
         );
