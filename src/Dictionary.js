fa4f9d75a853d4a236a8a61bd355b2348fd7ef48
Opeyemi <yemilokoro@gmail.com>
2023-08-23 22:57:10 -0400

    used sheCodes image api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,16 +8,26 @@ export default function Dictionary(props) {
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
 
-  function handleResponse(response) {
-    console.log(response.data[0]);
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handleScResponse(response) {
+    console.log(response);
   }
 
   function search() {
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    console.log(apiUrl);
-    axios.get(apiUrl).then(handleResponse);
+
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let scApiKey = "20a7b4d455458e7ffo4952342tabf625";
+    let scApiUrl = `https://api.shecodes.io/images/v1/search?query={advocate}&key={20a7b4d455458e7ffo4952342tabf625}`;
+
+    axios
+      .get(scApiUrl, { headers: { Authorization: `Bearer ${scApiKey}` } })
+      .then(handleScResponse);
   }
 
   function handleSubmit(event) {
